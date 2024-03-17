aaf3084cf33d53b448631c326891586259422989
florian2richter <florian2richter@gmail.com>
2024-03-17 17:12:47 +0100

    set up logistic regression in 1-D

--- scripts/logistic_regression.py
+++ scripts/logistic_regression.py
@@ -13,17 +13,55 @@ df["Class"] = df["Class"].replace({2: 0, 4: 1})
 # now start with the logistic regression ;)
 
 
-def logit(x, c):
-    return c + 1 / (1 + np.exp(-x))
+class LogisticRegression:
+    def __init__(self, a_0, b_0):
+        self.a = a_0
+        self.b = b_0
+        self.a_grad = 0
+        self.b_grad = 0
+
+    def logit(self, x):
+        return 1 / (1 + np.exp(-(self.a * x + self.b)))
+
+    def loss(self, pred, y):
+        return -np.mean(y * np.log(pred) + (1 - y) * np.log(1 - pred))
+
+    def gradient(self, pred, y, x):
+
+        a_grad = np.mean((pred - y) * x)
+        b_grad = np.mean(pred - y)
+
+        return a_grad, b_grad
+
+    def update(self, stepsize):
+        self.a = self.a * stepsize * self.a_grad
+        self.b = self.b * stepsize * self.b_grad
+        return
+
+    def train(self, x, y, stepsize=0.001):
+        pred = self.logit(x)
+        print("loss is")
+        print(self.loss(pred, y))
+        self.a_grad, self.b_grad = self.gradient(pred, x, y)
+        self.update(stepsize)
+        return
 
 
-def binary_cross_entropy(y, pred):
-    y * np.log(pred) + (1 - y) * np.log(1 - pred)
+# initialize
+logitistic_regressor = LogisticRegression(np.random.rand(), np.random.rand())
+x = df["Mitoses"]
+y = df["Class"]
+pred = logitistic_regressor.logit(x)
+# testplot
+print(logitistic_regressor.loss(pred, y))
 
+# now train the regression
 
-y = np.array([1, 0, 1, 1, 0])  # Ground truth labels
-pred = np.array([0.9, 0.1, 0.8, 0.7, 0.2])  # Model predictions
+for i in range(10):
+    print(f"Training step {i}")
+    logitistic_regressor.train(x, y)
 
-result = y * pred
-
-print(result)
+# y = np.array([1, 0, 1, 1, 0])  # Ground truth labels
+# pred = np.array([0.9, 0.1, 0.8, 0.7, 0.2])  # Model predictions
+#
+# plt.show()
