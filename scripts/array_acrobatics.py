37aa239b8548f1ebd1311c5c0d1ece3a3ac7c1d6
florian2richter <florian2richter@gmail.com>
2024-03-20 21:46:31 +0100

    implement a 2d convolution

---
+++ scripts/array_acrobatics.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def convolution2D(image,kernel):
+    offset_x = kernel.shape[0]//2
+    offset_y = kernel.shape[1]//2
+    result = []
+    for i in range(offset_x,image.shape[0]-offset_x):
+        for j in range(offset_y,:image.shape[1]-offset_y):
+            # here convolve
+            result.append(np.sum(image[i-offset_x:i+offset_x,j-offset_y:j+offset_y]*kernel))
+    return np.array(result)
+
+example = np.array([[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]])
+kernel = np.array([[0,0,0],[0,1,0],[0,0,0]])
+
+print(convolution2D(example, kernel))
