33efd510c9794d7dc7e8564e2e6e106a1670223b
florian2richter <florian2richter@gmail.com>
2024-03-22 19:53:45 +0100

    added plot

--- scripts/array_acrobatics.py
+++ scripts/array_acrobatics.py
@@ -6,18 +6,20 @@ def convolution2D(image, kernel):
     offset_y = kernel.shape[1] // 2
     result = []
     for i in range(offset_x, image.shape[0] - offset_x):
+        row = []
         for j in range(offset_y, image.shape[1] - offset_y):
             # here convolve
-            result.append(
-                np.sum(
-                    image[i - offset_x : i + offset_x, j - offset_y : j + offset_y]
-                    * kernel
-                )
-            )
+            image_snip = image[
+                i - offset_x : i + offset_x + 1, j - offset_y : j + offset_y + 1
+            ]
+            row.append(np.sum(image_snip * kernel))
+        result.append(row)
+
     return np.array(result)
 
 
 example = np.array([[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]])
 kernel = np.array([[0, 0, 0], [0, 1, 0], [0, 0, 0]])
 
-print(convolution2D(example, kernel))
+result = convolution2D(example, kernel)
+print(result)
